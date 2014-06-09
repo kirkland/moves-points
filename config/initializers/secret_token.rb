5ab54153a7130cdd46376338856f800aae7a0079
Robert Kaufman <rob.m.kaufman@gmail.com>
2014-06-09 07:43:32 -0400

    Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MovesPoints::Application.config.secret_key_base = '8ab5d23193d85d6862f81df5202d77ecf50a60bfc67a5fd68c35b70f9fc29d7921ef57733668b2bc00be442989b5a7b6d500a9918029acf09cb2dfaaac6f7924'
