7c41bee3d25c4e3a4ac87c22d8ca7c09107feb1a
Robert Kaufman <rob.m.kaufman@gmail.com>
2014-06-10 18:24:13 -0400

    Create summaries

--- app/models/summary.rb
+++ app/models/summary.rb
@@ -1,7 +1,14 @@
 class Summary < ActiveRecord::Base
   # Validations
+  validates :bicycling_distance, presence: true
   validates :date, presence: true
-  validates :distance, presence: true
-  validates :kind, presence: true
+  validates :running_distance, presence: true
   validates :user, presence: true
+  validates :walking_distance, presence: true
+
+  def points
+    (bicycling_distance * 10) +
+      (walking_distance * 30) +
+      (running_distance * 100)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,25 @@
 class User < ActiveRecord::Base
   # Associations
   has_many :oauth_tokens
+  has_many :summaries
+
+  def fetch_data_for_dates(start_date, end_date)
+    api = MovesApi.new(current_oauth_token)
+    (start_date..end_date).each do |date|
+      result = api.summary(date)
+
+      summaries.create! do |summary|
+        summary.date = date
+        summary.bicycling_distance = result[:bicycling_distance].presence.try(:to_i) || 0
+        summary.running_distance = result[:running_distance].presence.try(:to_i) || 0
+        summary.walking_distance = result[:walking_distance].presence.try(:to_i) || 0
+      end
+    end
+  end
+
+  private
+
+  def current_oauth_token
+    oauth_tokens.order(:created_at).last
+  end
 end
